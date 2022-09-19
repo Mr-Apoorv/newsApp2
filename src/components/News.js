4044f013988dce3b0a64fd489d5d228e4e86400a
Apoorv <dixit.a.apoorv@gmail.com>
2022-09-20 01:31:49 +0530

    L-24 Fetching API Key from News API

--- src/components/News.js
+++ src/components/News.js
@@ -4,14 +4,19 @@ import NewsItem from "./NewsItem";
 export class News extends Component {
   render() {
     return (
-      <div>
-        News
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
+      <div className="container my-3">
+        <h2 className="text-center">News Monkey App 🐒</h2>
+        <div className="row my-3">
+          <div className="col-md-4">
+            <NewsItem title="mytitle" desc="mydesc" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="mytitle" desc="mydesc" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="mytitle" desc="mydesc" />
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,7 +2,26 @@ import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
-    return <div>Newsitem 2</div>;
+    let { title, desc } = this.props;
+
+    return (
+      <div>
+        <div className="card" style={{ width: "18rem" }}>
+          <img
+            src="https://images.moneycontrol.com/static-mcnews/2022/06/Stocks-770x433.jpg"
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">{this.props.title}</h5>
+            <p className="card-text">{desc}.</p>
+            <a href="/newsDetails" className="btn btn-primary">
+              Read more
+            </a>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
